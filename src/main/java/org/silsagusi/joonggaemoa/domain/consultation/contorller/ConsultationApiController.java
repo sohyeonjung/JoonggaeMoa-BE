8b137200b54a73f2c2f91850c4b356f2823b62d2
sixbluebird <yook127@naver.com>
2025-03-24 12:09:37 +0900

    [#5]feat: Api명세대로 구현해야할 목록 작성

--- src/main/java/org/silsagusi/joonggaemoa/domain/consultation/contorller/ConsultationApiController.java
+++ src/main/java/org/silsagusi/joonggaemoa/domain/consultation/contorller/ConsultationApiController.java
@@ -5,21 +5,19 @@ import org.silsagusi.joonggaemoa.domain.consultation.contorller.dto.Consultation
 import org.silsagusi.joonggaemoa.domain.consultation.service.ConsultationService;
 import org.silsagusi.joonggaemoa.global.api.ApiResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequestMapping("/api/agents/{agentId}/consultations")
 @RequiredArgsConstructor
 public class ConsultationApiController {
 
 	private final ConsultationService consultationService;
 
 	//상담 예약
-	@PostMapping("/api/agents/{agentId}/consultations")
+	@PostMapping("")
 	public ResponseEntity<ApiResponse<ConsultationResponseDto>> createConsultation(
 		@PathVariable Long agentId,
 		@RequestBody ConsultationRequestDto consultationRequestDto
@@ -28,4 +26,25 @@ public class ConsultationApiController {
 			consultationRequestDto);
 		return ResponseEntity.ok(ApiResponse.created(responseDto));
 	}
+
+	//상담 리스트 조회
+//	@GetMapping("")
+
+	//상담 상태별 건수 조회
+//	@GetMapping("")
+
+	//상담 상세 조회
+//	@GetMapping("")
+
+	//상담 상세 작성
+//	@PatchMapping("{consultationId}")
+
+	//상담 상세 수정
+//	@PatchMapping("{consultationId}")
+
+	//상담 삭제
+//	@DeleteMapping("{consultationId}")
+
+	//상담 상태 변경
+//	@PatchMapping("{consultationId}")
 }
