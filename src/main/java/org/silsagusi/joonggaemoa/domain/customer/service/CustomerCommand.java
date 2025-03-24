847c547b1ca21e13a73369b7d272dd1d30b64231
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-03-24 15:44:58 +0900

    [#10] feat: 고객 등록 기능 추가

---
+++ src/main/java/org/silsagusi/joonggaemoa/domain/customer/service/CustomerCommand.java
@@ -0,0 +1,40 @@
+package org.silsagusi.joonggaemoa.domain.customer.service;
+
+import java.time.LocalDate;
+
+import org.silsagusi.joonggaemoa.domain.customer.entity.Customer;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CustomerCommand {
+	private Long id;
+	private String name;
+	private LocalDate birthday;
+	private String phone;
+	private String email;
+	private String job;
+	private Boolean isVip;
+	private String memo;
+	private Boolean consent;
+
+	public static CustomerCommand toCommand(Customer customer) {
+		return CustomerCommand.builder()
+			.id(customer.getId())
+			.name(customer.getName())
+			.birthday(customer.getBirthday())
+			.phone(customer.getPhone())
+			.email(customer.getEmail())
+			.job(customer.getJob())
+			.isVip(customer.getIsVip())
+			.memo(customer.getMemo())
+			.consent(customer.getConsent())
+			.build();
+	}
+}
