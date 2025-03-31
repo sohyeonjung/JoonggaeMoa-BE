c23fdbd02376dab6d8aea36982aec91205474812
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-03-31 18:05:36 +0900

    test: redis config 수수정

--- src/main/java/org/silsagusi/joonggaemoa/global/config/RedisConfig.java
+++ src/main/java/org/silsagusi/joonggaemoa/global/config/RedisConfig.java
@@ -12,7 +12,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
-	@Value("${spring.redis.host}")
+	@Value("${spring.data.redis.host}")
 	private String host;
 
 	@Value("${spring.data.redis.port}")
